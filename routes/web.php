83c1053305d441f2c3e7c1a6e8797421074fc0e9
Kokabelva <3103120125@student.smktelkom-pwt.sch.id>
2022-02-17 20:58:37 +0700

    upload modul 2

---
+++ routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+//Route::get('/', function () {
+  //  return view('welcome');
+//});
+
+Route::get('/', function() {
+    return view('home', [
+        "title" => "Home"
+    ]);
+});
+
+Route::get('/about', function() {
+    return view('about', [
+        "title" => "About",
+        "nama" => "Koka Belva Faustina ",
+        "email" => "3103120125@student.smktelkom-pwt.sch.id",
+        "gambar" => "Koka.jpg"
+    ]);
+});
+
+Route::get('/gallery', function() {
+    return view('gallery', [
+        "title" => "Gallery"
+    ]);
+});
+
